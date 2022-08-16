c340e34db058d332237005bfa4931c45875bdf27
Bob Belderbos <bobbelderbos@gmail.com>
2022-08-16 18:02:29 +0200

    code we wrote

--- script.py
+++ script.py
@@ -1,10 +1,15 @@
+from collections import Counter
 from datetime import datetime
 from typing import Optional
 
 from sqlmodel import Session, select
+from sqlmodel.sql.expression import Select, SelectOfScalar
 import typer
 
-from db import Activity, engine
+from db import Activity, engine, add_activity, stop_activity, cancel_activity, get_activities
+
+SelectOfScalar.inherit_cache = True  # type: ignore
+Select.inherit_cache = True  # type: ignore
 
 app = typer.Typer()
 
@@ -14,13 +19,7 @@ def start(name: str, interactive: bool = False):
     """
     Start timer for activity
     """
-    activity = Activity(
-        name=name,
-        start=datetime.now()
-    )
-    with Session(engine) as session:
-        session.add(activity)
-        session.commit()
+    add_activity(name)
 
 
 @app.command()
@@ -28,16 +27,7 @@ def stop(name: str):
     """
     Stop timer for activity
     """
-    with Session(engine) as session:
-        statement = select(Activity).where(
-            Activity.name == name,
-            Activity.end == None  # noqa E711
-        )
-        results = session.exec(statement)
-        for row in results:
-            row.end = datetime.now()
-            session.add(row)
-        session.commit()
+    stop_activity(name)
 
 
 @app.command()
@@ -46,6 +36,7 @@ def cancel(name: str):
     Cancel a timer for an activity in transit
     """
     print("cancel", name)
+    cancel_activity(name)
 
 
 @app.command()
@@ -54,6 +45,9 @@ def show(name: Optional[str] = typer.Argument(None)):
     Show timer for all activities or a specific activity
     """
     print("show", name)
+    activities = get_activities(name)
+    for name, total in activities.items():
+        print(name, total)
 
 
 @app.command()
@@ -62,6 +56,18 @@ def remove(name: str, all_entries: bool = False):
     Remove timer for an activity, last or all entries
     """
     print("remove", name, f"{all_entries=}")
+    with Session(engine) as session:
+        statement = select(Activity).where(
+            Activity.name == name,
+        )
+
+        results = session.exec(statement)
+        if not all_entries:
+            results = [results[-1]]
+
+        for row in results:
+            session.delete(row)
+        session.commit()
 
 
 if __name__ == "__main__":
